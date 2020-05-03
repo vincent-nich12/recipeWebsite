3feb14067b896b6eb54a2d4cda3b46d7fbb8f1d1
vrq16kku <vrq16kku@uea.ac.uk>
2020-05-03 17:05:57 +0100

    test

--- server/server.py
+++ server/server.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+#Homepage
 @app.route('/')
 def home():
-	#open website
-	return render_template('index.html')
+    return render_template('index.html')
+
+#Function to open a file
+def openFile(aFile):
+	with open(aFile) as inFile:
+		list = inFile.readlines()
+	return list
 
 if __name__ == '__main__':
-	app.run(debug=True, host="0.0.0.0")
+    
+    app.run(debug=True, host="0.0.0.0")
