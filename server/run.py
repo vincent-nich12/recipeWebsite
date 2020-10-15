e8dfe3c87b4508616a3dd81ad47d8992148efde5
root <vince1998@hotmail.co.uk>
2020-10-15 14:24:36 +0000

    test

--- server/run.py
+++ server/run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes import *
+import os
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=443, ssl_context=('../../../../etc/letsencrypt/live/nkrecipes.co.uk/fullchain.pem',
+    '../../../../etc/letsencrypt/live/nkrecipes.co.uk/privkey.pem'))
